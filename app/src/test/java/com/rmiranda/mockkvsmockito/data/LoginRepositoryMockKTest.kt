0b123a5dd114ed606caee4c57d709d7b439e91da
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2021-12-14 16:40:17 -0600

    ✅ Add LoginRepositoryMockKTest using MockK

---
+++ app/src/test/java/com/rmiranda/mockkvsmockito/data/LoginRepositoryMockKTest.kt
@@ -0,0 +1,42 @@
+package com.rmiranda.mockkvsmockito.data
+
+import com.rmiranda.mockkvsmockito.data.utils.TestLoginUtils.fakeUser
+import com.rmiranda.mockkvsmockito.data.utils.TestLoginUtils.password
+import com.rmiranda.mockkvsmockito.data.utils.TestLoginUtils.resultError
+import com.rmiranda.mockkvsmockito.data.utils.TestLoginUtils.userName
+import com.rmiranda.mockkvsmockito.data.utils.TestLoginUtils.wrongPassword
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * Created by rodrigomiranda on 14/12/21.
+ * Applaudo Studios
+ */
+class LoginRepositoryMockKTest {
+    private lateinit var loginRepository: LoginRepository
+    private lateinit var loginDataSource: LoginDataSource
+
+    @Before
+    fun setup() {
+        loginDataSource = mockk {
+            every { login(userName, password) } returns Result.Success(fakeUser)
+            every { login(userName, wrongPassword) } returns resultError
+        }
+        loginRepository = LoginRepository(loginDataSource)
+    }
+
+    @Test
+    fun `User logs in with correct credentials`() {
+        val result = loginRepository.login(userName, password)
+        Assert.assertEquals(result, Result.Success(fakeUser))
+    }
+
+    @Test
+    fun `User logs in with incorrect credentials`() {
+        val result = loginRepository.login(userName, wrongPassword)
+        Assert.assertEquals(result, resultError)
+    }
+}
