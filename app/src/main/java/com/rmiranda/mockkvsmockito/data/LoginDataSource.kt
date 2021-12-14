d9ddca08688128bc56cd12c92e9337ebc3bc221e
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2021-12-14 17:30:45 -0600

    ✨ Add suspend function to LoginDataSource

--- app/src/main/java/com/rmiranda/mockkvsmockito/data/LoginDataSource.kt
+++ app/src/main/java/com/rmiranda/mockkvsmockito/data/LoginDataSource.kt
@@ -1,6 +1,8 @@
 package com.rmiranda.mockkvsmockito.data
 
 import com.rmiranda.mockkvsmockito.data.model.LoggedInUser
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.IOException
 
 /**
@@ -20,5 +22,12 @@ class LoginDataSource {
 
     fun logout() {
         // TODO: revoke authentication
+    }
+
+    suspend fun fetchUserUrl(): String {
+        withContext(Dispatchers.IO) {
+            Thread.sleep(1000)
+        }
+        return "https://user.com"
     }
 }
